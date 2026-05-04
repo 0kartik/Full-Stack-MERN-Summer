e2cc5c056edc4f333ae4284ef9ccf07d21143edc
Kartikeya <janardanagnihotram@gmail.com>
2026-05-04 14:27:34 +0530

    commit

--- calculator.js
+++ calculator.js
@@ -5,17 +5,22 @@ let op="";
 switch(op){
     case "+":
         console.log(num1+num2);
+        break;
     case "-":
         if (num1>num2){
             console.log(num1-num2);
+            break;
         }
         else{
             console.log(num2-num1);
+            break;
         }
     case "x":
         console.log(num1*num2);
+        break;
     case "/":
         console.log(num1/num2);
+        break;
     default:
         console.log("Enter valid operator");
 }
