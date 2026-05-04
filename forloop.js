76bdd3476d35bcb0baf10c96c4dcce06b7ba84af
Kartikeya <janardanagnihotram@gmail.com>
2026-05-04 14:42:06 +0530

    commit

---
+++ forloop.js
@@ -0,0 +1,18 @@
+// factorial
+
+let n=5
+let pro=1
+for (let i=n;i>0;i--){
+    pro=pro*i
+}
+console.log(pro)
+
+//string reversal 
+
+let s="kartik"
+let m=s.length
+let rev=""
+for (let i=m-1;i>=0;i--){
+    rev+=s[i]
+}
+console.log(rev)
